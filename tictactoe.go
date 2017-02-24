b04db4cbe9234540b9c356b1f81c17855f700da7
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-24 01:38:34 -0600

    Draw some dummy board

--- tictactoe.go
+++ tictactoe.go
@@ -22,5 +22,174 @@
 
 package main
 
+import (
+	"fmt"
+	"github.com/nsf/termbox-go"
+	"os"
+)
+
+func drawBoard() {
+	colorDefault := termbox.ColorDefault
+
+	// -------------
+	// | O | X | O |
+	// -------------
+	// | O | X | O |
+	// -------------
+	// | O | X | O |
+	// -------------
+
+	// TODO: refactor this code
+
+	// top
+	for x := 1; x <= 13; x++ {
+		var c rune
+		if x == 1 {
+			c = '\u2554'
+		} else if x == 13 {
+			c = '\u2557'
+		} else {
+			c = '\u2550'
+		}
+		termbox.SetCell(x, 1, c, colorDefault, colorDefault)
+	}
+
+	// left
+	for y := 2; y <= 6; y++ {
+		var c rune
+		c = '\u2551'
+		termbox.SetCell(1, y, c, colorDefault, colorDefault)
+	}
+
+	// bottom
+	for x := 1; x <= 13; x++ {
+		var c rune
+		if x == 1 {
+			c = '\u255A'
+		} else if x == 13 {
+			c = '\u255D'
+		} else {
+			c = '\u2550'
+		}
+		termbox.SetCell(x, 7, c, colorDefault, colorDefault)
+	}
+
+	// right
+	for y := 2; y <= 6; y++ {
+		var c rune
+		c = '\u2551'
+		termbox.SetCell(13, y, c, colorDefault, colorDefault)
+	}
+
+	// first horizontal line
+	for x := 1; x <= 13; x++ {
+		var c rune
+		if x == 1 {
+			c = '\u2560'
+		} else if x == 13 {
+			c = '\u2563'
+		} else {
+			c = '\u2550'
+		}
+		termbox.SetCell(x, 3, c, colorDefault, colorDefault)
+	}
+
+	// second horizontal line
+	for x := 1; x <= 13; x++ {
+		var c rune
+		if x == 1 {
+			c = '\u2560'
+		} else if x == 13 {
+			c = '\u2563'
+		} else {
+			c = '\u2550'
+		}
+		termbox.SetCell(x, 5, c, colorDefault, colorDefault)
+	}
+
+	// first vertical line
+	for y := 1; y <= 7; y++ {
+		var c rune
+		if y == 1 {
+			c = '\u2566'
+		} else if y == 3 || y == 5 {
+			c = '\u256C'
+		} else if y == 7 {
+			c = '\u2569'
+		} else {
+			c = '\u2551'
+		}
+		termbox.SetCell(5, y, c, colorDefault, colorDefault)
+	}
+
+	// second vertical line
+	for y := 1; y <= 7; y++ {
+		var c rune
+		if y == 1 {
+			c = '\u2566'
+		} else if y == 3 || y == 5 {
+			c = '\u256C'
+		} else if y == 7 {
+			c = '\u2569'
+		} else {
+			c = '\u2551'
+		}
+		termbox.SetCell(9, y, c, colorDefault, colorDefault)
+	}
+
+	termbox.SetCell(3, 2, 'O', colorDefault, colorDefault)
+	termbox.SetCell(7, 2, 'X', colorDefault, colorDefault)
+	termbox.SetCell(11, 2, 'X', colorDefault, colorDefault)
+	termbox.SetCell(3, 4, 'O', colorDefault, colorDefault)
+	termbox.SetCell(7, 4, 'X', colorDefault, colorDefault)
+	termbox.SetCell(11, 4, 'O', colorDefault, colorDefault)
+	termbox.SetCell(3, 6, 'X', colorDefault, colorDefault)
+	termbox.SetCell(7, 6, 'X', colorDefault, colorDefault)
+	termbox.SetCell(11, 6, 'O', colorDefault, colorDefault)
+}
+
+func redrawAll() {
+	colorDefault := termbox.ColorDefault
+	termbox.Clear(colorDefault, colorDefault)
+
+	drawBoard()
+
+	termbox.Flush()
+}
+
+func runGame() {
+	err := termbox.Init()
+	if err != nil {
+		errorAndExit(err)
+	}
+	defer termbox.Close()
+
+	eventQueue := make(chan termbox.Event)
+	go func() {
+		for {
+			eventQueue <- termbox.PollEvent()
+		}
+	}()
+
+	redrawAll()
+exitGame:
+	for {
+		select {
+		case ev := <-eventQueue:
+			switch ev.Key {
+			case termbox.KeyEsc:
+				break exitGame
+			}
+		}
+		redrawAll()
+	}
+}
+
+func errorAndExit(message interface{}) {
+	fmt.Println(message)
+	os.Exit(1)
+}
+
 func main() {
+	runGame()
 }
