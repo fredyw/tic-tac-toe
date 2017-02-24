cca54a1c04daf690ee18aa50d2ba47eb9abdee27
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-24 01:55:15 -0600

    Update some code

--- tictactoe.go
+++ tictactoe.go
@@ -28,20 +28,20 @@ import (
 	"os"
 )
 
+func drawText(x, y int, text string) {
+	colorDefault := termbox.ColorDefault
+	for _, ch := range text {
+		termbox.SetCell(x, y, ch, colorDefault, colorDefault)
+		x++
+	}
+}
+
 func drawBoard() {
 	colorDefault := termbox.ColorDefault
 
-	// -------------
-	// | O | X | O |
-	// -------------
-	// | O | X | O |
-	// -------------
-	// | O | X | O |
-	// -------------
-
 	// TODO: refactor this code
 
-	// top
+	// first horizontal line
 	for x := 1; x <= 13; x++ {
 		var c rune
 		if x == 1 {
@@ -51,47 +51,7 @@ func drawBoard() {
 		} else {
 			c = '\u2550'
 		}
-		termbox.SetCell(x, 1, c, colorDefault, colorDefault)
-	}
-
-	// left
-	for y := 2; y <= 6; y++ {
-		var c rune
-		c = '\u2551'
-		termbox.SetCell(1, y, c, colorDefault, colorDefault)
-	}
-
-	// bottom
-	for x := 1; x <= 13; x++ {
-		var c rune
-		if x == 1 {
-			c = '\u255A'
-		} else if x == 13 {
-			c = '\u255D'
-		} else {
-			c = '\u2550'
-		}
-		termbox.SetCell(x, 7, c, colorDefault, colorDefault)
-	}
-
-	// right
-	for y := 2; y <= 6; y++ {
-		var c rune
-		c = '\u2551'
-		termbox.SetCell(13, y, c, colorDefault, colorDefault)
-	}
-
-	// first horizontal line
-	for x := 1; x <= 13; x++ {
-		var c rune
-		if x == 1 {
-			c = '\u2560'
-		} else if x == 13 {
-			c = '\u2563'
-		} else {
-			c = '\u2550'
-		}
-		termbox.SetCell(x, 3, c, colorDefault, colorDefault)
+		termbox.SetCell(x, 0, c, colorDefault, colorDefault)
 	}
 
 	// second horizontal line
@@ -104,17 +64,50 @@ func drawBoard() {
 		} else {
 			c = '\u2550'
 		}
-		termbox.SetCell(x, 5, c, colorDefault, colorDefault)
+		termbox.SetCell(x, 2, c, colorDefault, colorDefault)
+	}
+
+	// third horizontal line
+	for x := 1; x <= 13; x++ {
+		var c rune
+		if x == 1 {
+			c = '\u2560'
+		} else if x == 13 {
+			c = '\u2563'
+		} else {
+			c = '\u2550'
+		}
+		termbox.SetCell(x, 4, c, colorDefault, colorDefault)
+	}
+
+	// fourth horizontal line
+	for x := 1; x <= 13; x++ {
+		var c rune
+		if x == 1 {
+			c = '\u255A'
+		} else if x == 13 {
+			c = '\u255D'
+		} else {
+			c = '\u2550'
+		}
+		termbox.SetCell(x, 6, c, colorDefault, colorDefault)
 	}
 
 	// first vertical line
-	for y := 1; y <= 7; y++ {
+	for y := 1; y <= 5; y++ {
 		var c rune
-		if y == 1 {
+		c = '\u2551'
+		termbox.SetCell(1, y, c, colorDefault, colorDefault)
+	}
+
+	// second vertical line
+	for y := 0; y <= 6; y++ {
+		var c rune
+		if y == 0 {
 			c = '\u2566'
-		} else if y == 3 || y == 5 {
+		} else if y == 2 || y == 4 {
 			c = '\u256C'
-		} else if y == 7 {
+		} else if y == 6 {
 			c = '\u2569'
 		} else {
 			c = '\u2551'
@@ -122,14 +115,14 @@ func drawBoard() {
 		termbox.SetCell(5, y, c, colorDefault, colorDefault)
 	}
 
-	// second vertical line
-	for y := 1; y <= 7; y++ {
+	// third vertical line
+	for y := 0; y <= 6; y++ {
 		var c rune
-		if y == 1 {
+		if y == 0 {
 			c = '\u2566'
-		} else if y == 3 || y == 5 {
+		} else if y == 2 || y == 4 {
 			c = '\u256C'
-		} else if y == 7 {
+		} else if y == 6 {
 			c = '\u2569'
 		} else {
 			c = '\u2551'
@@ -137,15 +130,22 @@ func drawBoard() {
 		termbox.SetCell(9, y, c, colorDefault, colorDefault)
 	}
 
-	termbox.SetCell(3, 2, 'O', colorDefault, colorDefault)
-	termbox.SetCell(7, 2, 'X', colorDefault, colorDefault)
-	termbox.SetCell(11, 2, 'X', colorDefault, colorDefault)
-	termbox.SetCell(3, 4, 'O', colorDefault, colorDefault)
-	termbox.SetCell(7, 4, 'X', colorDefault, colorDefault)
-	termbox.SetCell(11, 4, 'O', colorDefault, colorDefault)
-	termbox.SetCell(3, 6, 'X', colorDefault, colorDefault)
-	termbox.SetCell(7, 6, 'X', colorDefault, colorDefault)
-	termbox.SetCell(11, 6, 'O', colorDefault, colorDefault)
+	// fourth
+	for y := 1; y <= 5; y++ {
+		var c rune
+		c = '\u2551'
+		termbox.SetCell(13, y, c, colorDefault, colorDefault)
+	}
+
+	termbox.SetCell(3, 1, 'O', colorDefault, colorDefault)
+	termbox.SetCell(7, 1, 'X', colorDefault, colorDefault)
+	termbox.SetCell(11, 1, 'X', colorDefault, colorDefault)
+	termbox.SetCell(3, 3, 'O', colorDefault, colorDefault)
+	termbox.SetCell(7, 3, 'X', colorDefault, colorDefault)
+	termbox.SetCell(11, 3, 'O', colorDefault, colorDefault)
+	termbox.SetCell(3, 5, 'X', colorDefault, colorDefault)
+	termbox.SetCell(7, 5, 'X', colorDefault, colorDefault)
+	termbox.SetCell(11, 5, 'O', colorDefault, colorDefault)
 }
 
 func redrawAll() {
@@ -153,6 +153,9 @@ func redrawAll() {
 	termbox.Clear(colorDefault, colorDefault)
 
 	drawBoard()
+	drawText(15, 1, "Player 1's turn:")
+	drawText(15, 2, "(1, 2, 3, 4, 5, 6, 7, 8, 9)")
+	drawText(2, 7, "Created by Fredy Wijaya")
 
 	termbox.Flush()
 }
